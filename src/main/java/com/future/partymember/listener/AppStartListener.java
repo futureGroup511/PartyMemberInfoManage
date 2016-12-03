4de6c130290975822d4ef6795bdf4775008d9170
songminju123 <860080937@qq.com>
2016-12-03 15:02:46 +0800

    添加启动监听器

---
+++ src/main/java/com/future/partymember/listener/AppStartListener.java
@@ -0,0 +1,36 @@
+package com.future.partymember.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * Application Lifecycle Listener implementation class AppStartListener
+ *
+ */
+@WebListener
+public class AppStartListener implements ServletContextListener {
+
+    /**
+     * Default constructor. 
+     */
+    public AppStartListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent arg0)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent arg0)  { 
+         String rootPath=arg0.getServletContext().getContextPath()+"/";
+         arg0.getServletContext().setAttribute("rootPath", rootPath);
+    }
+	
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,8 @@
 <html>
 <body>
 <h2>Hello World!</h2>
+
+<a href="manage/index">manage</a>
+
 </body>
 </html>
