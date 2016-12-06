b2dd7a7df8da505eab50ecd0ec2eda7de1d145e7
ding <1524334009@qq.com>
2016-12-06 20:17:04 +0800

    s

--- src/main/java/com/future/partymember/dao/impl/ManagerInfoDaoImpl.java
+++ src/main/java/com/future/partymember/dao/impl/ManagerInfoDaoImpl.java
@@ -7,8 +7,7 @@ import org.springframework.stereotype.Repository;
 import com.future.partymember.base.BaseDao;
 import com.future.partymember.dao.IManagerInfoDao;
 import com.future.partymember.entity.ManagerInfo;
-import com.future.partymember.entity.PartyMemberInfo;
-import com.future.partymember.entity.PartySecretaryInfo;
+
 
 @Repository
 public class ManagerInfoDaoImpl extends BaseDao<ManagerInfo> implements IManagerInfoDao {
