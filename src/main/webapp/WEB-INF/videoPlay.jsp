ab27b45231a0e966470f912da3259ebe71c0eaec
ding <1524334009@qq.com>
2016-12-10 16:06:21 +0800

    改了一下视频播放的方式

--- src/main/webapp/WEB-INF/videoPlay.jsp
+++ src/main/webapp/WEB-INF/videoPlay.jsp
@@ -6,13 +6,12 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>${video.name}</title>
 </head>
-<body onbeforeunload="return stopPlay()">
+<body>
 		<video width="320" height="240" controls  onplay="startVideo()">
 		  <source src="${rootPath }${video.videoUrl}"  type="video/mp4">
 		  您的浏览器不支持 HTML5 video 标签。
 		</video>
 		
-		<button id="stop-play"></button>
 </body>
 <script type="text/javascript" src="${rootPath }js/jquery-3.1.1.min.js"></script>
 <script type="text/javascript" src="${rootPath }js/learn_video_date.js"></script>
--- src/main/webapp/js/learn_video_date.js
+++ src/main/webapp/js/learn_video_date.js
@@ -1,19 +1,34 @@
 
+	var count =0;//计数器
+	var start ;//视频开始
+	var end ;//视频结束
+	var watchLength;//观看时长
+
 $(document).ready(function() {
-	countTime();
+	startVideo();
+	window.onbeforeunload = onbeforeunload_handler;  
 });
 
 
-function countTime() {
-	$.ajax({
-		url: "/PartyMemberInfoManage/party/partyMemberAction_updateLearnTime",
-		type: 'POST',
-		dataType: 'json',
-		
-		data:{"time":60},
-		success: function(data){
-		}
-	});
-
-	setTimeout('countTime()', 60000);    //60秒更新
+function startVideo(){
+	if (count==0) {
+		start = new Date();
+		count=count+1;
+	}
 }
+
+function onbeforeunload_handler(){
+	end = new Date();
+	watchLength = end.getTime()-start.getTime();//这是毫秒
+	
+    $.ajax({
+    	url: "/PartyMemberInfoManage/party/partyMemberAction_updateLearnTime",
+    		
+    	type: 'POST',
+    	dataType: 'json',
+    	data:{"time":watchLength},
+    });   
+}  
+
+
+  
