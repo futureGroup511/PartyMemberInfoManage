d8b04ee27f5799c1134fba12e0fb620a4cf22436
jxy <1062100557@qq.com>
2016-12-10 20:37:32 +0800

    时间转换2

--- src/main/java/com/future/partymember/util/SwitchTime.java
+++ src/main/java/com/future/partymember/util/SwitchTime.java
@@ -8,6 +8,14 @@ public class SwitchTime {
 		long hours = (time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60);
 		long minutes = (time % (1000 * 60 * 60)) / (1000 * 60);
 		long seconds = (time % (1000 * 60)) / 1000;
-		return hours + "小时" + minutes + "分钟" + seconds + "秒";
+		if(minutes==0){
+			return seconds + "秒";
+		}
+		else if(hours==0){
+			return minutes + "分钟" + seconds + "秒";
+		}
+		else{
+			return hours + "小时" + minutes + "分钟" + seconds + "秒";
+		}		
 	}
 }
