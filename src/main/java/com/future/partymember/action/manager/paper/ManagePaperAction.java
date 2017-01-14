4ef7797a7b6fa6fbe24de489fe71f9415121cedc
ding <1524334009@qq.com>
2017-01-14 10:57:57 +0800

    qq

--- src/main/java/com/future/partymember/action/manager/paper/ManagePaperAction.java
+++ src/main/java/com/future/partymember/action/manager/paper/ManagePaperAction.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import com.future.partymember.base.BaseAction;
 import com.future.partymember.entity.RedPaper;
 import com.future.partymember.entity.RedPaperType;
+import com.future.partymember.util.PageCut;
 import com.future.partymember.util.SwitchTime;
 
 /**
@@ -25,7 +26,43 @@ public class ManagePaperAction extends BaseAction{
 	private static final long serialVersionUID = 1L;
 	
 	private RedPaper redPaper;
+	private int page;
+
+	//删除文章
+	public String deletePaper() throws Exception{
+		String id=this.getRequest().getParameter("rp_Id");
+		if(id.equals("")){
+			this.getRequest().setAttribute("dTag", "删除失败");
+		}else{
+			redPaperService.deleteRedPaper(Integer.parseInt(id));
+			this.getRequest().setAttribute("dTag", "删除成功");
+		}
+		return  "managePaper";
+	}
 	
+	
+	//修改文章
+	public String updatePaper()  throws Exception{
+
+		RedPaperType r=redPaperTypeService.findById(redPaper.getPaperTypeId());
+		redPaper.setPaperType(r.getPaperType());
+		redPaperService.updateRedPaper(redPaper);
+		this.getRequest().setAttribute("p", "修改成功,如果您不满意,可继续修改");
+		this.getRequest().setAttribute("paper",redPaper);		
+		return "updatePaper";
+	}
+	
+	
+	//分页
+	public String paper() throws Exception{
+		if(page==0){
+			page=1;
+		}
+		
+		PageCut<RedPaper> pc=redPaperService.getPCByNew(page, 2);
+		this.getRequest().setAttribute("pc", pc);
+		return "managePaper";
+	}
 
 
 	//添加文章
@@ -45,7 +82,14 @@ public class ManagePaperAction extends BaseAction{
 	public void setRedPaper(RedPaper redPaper) {
 		this.redPaper = redPaper;
 	}
-	
+	public int getPage() {
+		return page;
+	}
+
+	public void setPage(int page) {
+		this.page = page;
+	}
+
 	
 
 
