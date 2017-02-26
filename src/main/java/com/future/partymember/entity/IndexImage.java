e6cb9834567af72ea66c5a6ddb85b738ab48eba9
宋民举 <860080937@qq.com>
2017-02-26 08:58:46 +0800

    添加轮播图片类“ ; ； ; :

---
+++ src/main/java/com/future/partymember/entity/IndexImage.java
@@ -0,0 +1,27 @@
+package com.future.partymember.entity;
+
+import java.util.Date;
+
+public class IndexImage {
+	private int id;
+	private String url;
+	private Date createTime;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public Date getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+}
---
+++ src/main/java/com/future/partymember/service/impl/IndexImageDao.java
@@ -0,0 +1,38 @@
+package com.future.partymember.service.impl;
+
+import java.util.List;
+
+import com.future.partymember.base.BaseDao;
+import com.future.partymember.entity.IndexImage;
+import com.future.partymember.service.IIndexImgService;
+
+public class IndexImageDao extends BaseDao<IndexImage> implements IIndexImgService{
+
+	String hql1="from IndexImage order by id desc";
+	
+	@Override
+	public List<IndexImage> getByNew(int num) {
+		// TODO Auto-generated method stub
+		return this.getEntityLimitList(hql1, 0, num);
+	}
+
+	@Override
+	public boolean addIndexImg(IndexImage indexImage) {
+		// TODO Auto-generated method stub
+		return this.saveEntity(indexImage);
+	}
+
+	@Override
+	public IndexImage getById(int id) {
+		// TODO Auto-generated method stub
+		return this.getEntity(id);
+	}
+
+	@Override
+	public boolean deleteById(int id) {
+		// TODO Auto-generated method stub
+		this.deleteEntity(this.getEntity(id));
+		return true;
+	}
+
+}
