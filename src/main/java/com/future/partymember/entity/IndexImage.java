1a9461a377919b18dcd380e1bc671b77d8398aa8
宋民举 <860080937@qq.com>
2017-02-26 09:01:05 +0800

    图片轮播类建立完成“

--- src/main/java/com/future/partymember/entity/IndexImage.java
+++ src/main/java/com/future/partymember/entity/IndexImage.java
@@ -1,10 +1,24 @@
 package com.future.partymember.entity;
 
+import static javax.persistence.GenerationType.IDENTITY;
+
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_indexImage")
 public class IndexImage {
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
 	private int id;
-	private String url;
+	
+	@Column(length=200)
+	private String url;//图片对应的链接,需要包括http://
 	private Date createTime;
 	public int getId() {
 		return id;
